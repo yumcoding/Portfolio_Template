3910939fe29d7328cc6795224149f991e9382585
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-08-06 13:35:28 +0200

    show navbar on scroll

---
+++ main.js
@@ -0,0 +1,16 @@
+const nav = document.getElementById("nav");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 580 ||
+    document.documentElement.scrollTop > 580
+  ) {
+    nav.style.top = "0";
+  } else {
+    nav.style.top = "-75px";
+  }
+}
