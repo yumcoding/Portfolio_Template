0bf9f1d9ae2f8d6cf392b2fc99ccffcd487b7472
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-08-07 11:15:01 +0200

    add project Array

---
+++ script/modal.js
@@ -0,0 +1,74 @@
+const projectArr = [
+  (project1 = {
+    id: "project1",
+    title: "LOREM, IPSUM DOLOR.",
+    tag: "UI/UX",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Magna quia Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus inventore",
+    url: "image/portfolio/item1.png",
+  }),
+  (project2 = {
+    id: "project2",
+    title: "LOREM, IPSUM DOLOR.",
+    tag: "HTML/CSS",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Magna quia Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus inventore",
+    url: "image/portfolio/item2.png",
+  }),
+  (project3 = {
+    id: "project3",
+    title: "LOREM, IPSUM DOLOR.",
+    tag: "VANILA JS",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Magna quia Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus inventore",
+    url: "image/portfolio/item3.png",
+  }),
+  (project4 = {
+    id: "project4",
+    title: "LOREM, IPSUM DOLOR.",
+    tag: "HTML/CSS/JS",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Magna quia Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus inventore",
+    url: "image/portfolio/item4.png",
+  }),
+  (project5 = {
+    id: "project5",
+    title: "LOREM, IPSUM DOLOR.",
+    tag: "React",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Magna quia Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus inventore",
+    url: "image/portfolio/item5.png",
+  }),
+  (project6 = {
+    id: "project6",
+    title: "LOREM, IPSUM DOLOR.",
+    tag: "React",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Magna quia Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus inventore",
+    url: "image/portfolio/item6.png",
+  }),
+  (project7 = {
+    id: "project7",
+    title: "LOREM, IPSUM DOLOR.",
+    tag: "React Native",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Magna quia Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus inventore",
+    url: "image/portfolio/item7.png",
+  }),
+  (project8 = {
+    id: "project8",
+    title: "LOREM, IPSUM DOLOR.",
+    tag: "Node JS",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Magna quia Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus inventore",
+    url: "image/portfolio/item8.png",
+  }),
+  (project9 = {
+    id: "project9",
+    title: "LOREM, IPSUM DOLOR.",
+    tag: "Node JS",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Magna quia Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus inventore",
+    url: "image/portfolio/item9.png",
+  }),
+];
